eb2533f52e6612d1a8ed52c7dfabd6de740e4bd5
MANIK2014 <tiwarisunu@gmail.com>
2026-06-09 17:41:01 +0100

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,29 @@
+tempreture=int(input("Enter tempreture:"))
+if tempreture==0:
+    print("It is freezing")
+elif tempreture<=9:
+    print("It's cold!!")    
+elif tempreture<=19:    
+    print("Ok,It is cold ,I recommend wearing warm clothes.")
+elif tempreture<=29:
+    print("Ok,It's warm,light clothes are suitable.") 
+elif tempreture<=30:
+    print("Oh it's really hot,You should go to the beach!!🏝️.")
+else:    
+    print("Oh!!!It's summer baby☀️🏝️😎")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #Note:The tempretures are written in celsius
